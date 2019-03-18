a3a8e60d2b4a7e037173d1ba00e5b0595ce0230f
Tito Griné <tito9992gmail.com>
2019-03-18 10:50:54 +0000

    Implemented first project goal.

--- proj/forensic.c
+++ proj/forensic.c
@@ -8,17 +8,112 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <stdbool.h>
+
+#define  BIT(n)         (0x01<<(n))
+
+struct OPTIONS {
+    bool check_subdir;
+    bool check_fingerprint;
+    int8_t fp_mask; // mask = bit(2) = md5 ; bit(1) = sha1 ; bit(0) = sha256
+    bool logfile;
+    char* logfilename;
+    bool output;
+    char* output_file;
+
+} options;
+
+bool check_output(char* name){
+    //char* extension;
+
+    if(name[0] == '-')
+        return false;
+
+    //extension = strstr(name, ".csv");
+
+    //TODO: Further extension verification
+
+    return true;
+}
+
+int parse_fingerprints(char* fingerprints){
+    char* token = strtok(fingerprints, ",");
+
+    while(token != NULL){
+        
+        if(strcmp(token, "md5") == 0){
+            options.fp_mask |= BIT(2);
+        } else if(strcmp(token, "sha1") == 0){
+            options.fp_mask |= BIT(1);
+        } else if(strcmp(token, "sha256") == 0){
+            options.fp_mask |= BIT(0);
+        } else {
+            return -1;
+        }
+
+        token = strtok(NULL, ",");
+    }
+
+    return 0;
+}
+
+int parse_options(int argc, char* argv[]){
+    int opt;
+
+    while((opt = getopt(argc, argv, "rh:o:v")) != -1){
+
+        switch(opt) {
+
+            case 'r':
+                options.check_subdir = true;
+                break;
+
+            case 'h':
+                options.check_fingerprint = true;
+                parse_fingerprints(optarg);          
+                break;
+
+            case 'o':
+                options.output = true;
+                if(check_output(optarg))
+                    options.output_file = optarg;
+                else
+                    return 1;
+                
+                break;
+
+            case 'v':
+                options.logfile = true;
+                options.logfilename = getenv("LOGFILENAME");
+                break;
+
+            default:
+                exit(69);
+        }
+    }
+
+    return 0;
+}
 
 
 int main(int argc, char* argv[], char* envp[]){
-
-    char *str;
 
     if (argc < 2)
     {
         fprintf( stderr, "Usage: %s dir_name\n", argv[0]);
         exit(1);
     }
+
+    if(parse_options(argc, argv) == 1)
+        exit(2);
+
+/*
+    if(fork() == 0){
+        execlp("file", "file", argv[1], NULL);
+        exit(2);
+    }
+
+*/
 
     exit(0);
 }
