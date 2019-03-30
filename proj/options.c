25f47d06a6360cb53dc9f7923723681aeb247c07
Tito Griné <tito999@gmail.com>
2019-03-30 11:58:57 +0000

    Added Makefille and fixed bug with extension verification

--- proj/options.c
+++ proj/options.c
@@ -22,24 +22,37 @@ static int parse_fingerprints(char* fingerprints, struct options* options) {
     return 0;
 }
 
-static bool check_argument(char* name) {
+static bool check_argument(char* filename) {
 
-    if(name[0] == '-')
+    // Checks if string has minimum number of character to be valid ( < strlen(a.txt))
+    if(strlen(filename) < 5)
         return false;
+
+    char* name = malloc(sizeof(char)*strlen(filename) + 1);
+    strcpy(name, filename);
+
+    if(name[0] == '-'){
+        free(name);
+        return false;
+    }
 
     char* token = strtok(name, ".");
 
     token = strtok(NULL, ".");
 
     // Checks if it has a valid extension (strcmp returns 0 if equal)
-    if(strcmp(token, "csv") && strcmp(token, "txt"))
+    if(strcmp(token, "csv") && strcmp(token, "txt")){
+        free(name);
         return false;
-
+    }
 
     // Checks if file name is valid
-    if((token = strtok(NULL, ".")) != NULL)
+    if((token = strtok(NULL, ".")) != NULL){
+        free(name);    
         return false;
+    }
 
+    free(name);
     return true;
 }
 
@@ -80,7 +93,7 @@ int parse_options(int argc, char* argv[], struct options* options){
 
                 if(check_argument(optarg)) {
                     options->output_file = optarg;
-                    options->output_fd = open(optarg, O_WRONLY | O_CREAT | O_EXCL, OUTPUT_OPEN_MODE);
+                    options->output_fd = open(options->output_file, O_WRONLY | O_CREAT | O_EXCL, OUTPUT_OPEN_MODE);
                     if (options->output_fd == -1) {
                         perror("output file open");
                         exit(3);
@@ -93,18 +106,18 @@ int parse_options(int argc, char* argv[], struct options* options){
 
             case 'v':
                 options->logfile = true;
-
                 options->logfilename = getenv("LOGFILENAME");
 
-                if(options->logfilename == NULL)
-                    return 3;
-                else
+                if(check_argument(options->logfilename)) {
                     options->logfilename_fd = open(options->logfilename, O_WRONLY | O_CREAT | O_EXCL, OUTPUT_OPEN_MODE);
-                
-                if(options->logfilename_fd == -1) {
+                    
+                    if(options->logfilename_fd == -1) {
                         perror("Log file open");
                         exit(3);
+                    }
                 }
+                else
+                    exit(3);
 
                 break;
 
